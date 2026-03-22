6e406b12e11305d04ff6d6ab53f055089d8b5054
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-03-22 14:02:51 -0300

    o meu primeiro programa já feito

---
+++ c/first.c
@@ -0,0 +1,13 @@
+//Meu primeiro programa feito, aprendi com o professor da faculdade, no caso é uma réplica visto que estava utilizando onlineGDB para digitar
+//Fiquei muito feliz naquele momento e estou muito feliz fazer ele novamente.
+#include <stdio.h>
+
+int main()
+{
+    char nome[] = "Gustavo";
+    int idade = 21;
+    float altura =1.71;
+    printf("Meu nome é %s, minha idade é %d e tenho %.2f.",nome, idade, altura);
+
+    return 0;
+}
