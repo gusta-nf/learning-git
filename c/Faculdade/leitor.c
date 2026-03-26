d16f5977b2b90d107009d2bf94e4a144e3bdf84c
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-03-26 20:06:10 -0300

    Aprendendo no material da faculdade o comando scanf

---
+++ c/Faculdade/leitor.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int numero;
+    printf("Entre com um número inteiro:\n");
+    scanf("%d", &numero);
+    printf("O valor informado pelo usuário foi %d\n", numero);
+    return 0;
+}
