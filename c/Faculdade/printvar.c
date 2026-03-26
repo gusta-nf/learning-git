3d7cf1195b3101a6826a6fa951c015dea751a0f0
gustavo_fernandez <229655762+gusta-nf@users.noreply.github.com>
2026-03-26 19:36:36 -0300

    Praticando váriaveis no C com o material da faculdade

---
+++ c/Faculdade/printvar.c
@@ -0,0 +1,9 @@
+// Treinando com o material da faculdade a utilizar variáveis na linguaguem C
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    char ch = 'Z';
+    printf("Atualemnte temos a = %d e ch = %c.\n", a, ch);
+    return(0);
+}
